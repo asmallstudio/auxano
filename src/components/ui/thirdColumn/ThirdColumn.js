4f05de8c1fc4facec2f731628b2dbcca60e8ef8c
Andrew Garcia <runandrew@gmail.com>
2018-05-28 19:19:11 -0700

    add padding

--- src/components/ui/thirdColumn/ThirdColumn.js
+++ src/components/ui/thirdColumn/ThirdColumn.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import styles from "./thirdColumn.scss";
+
 class ThirdColumn extends React.Component {
   render() {
     const { title, children } = this.props;
     return (
-      <div className="col-xs-12 col-lg-4">
+      <div className={`col-xs-12 col-lg-4 ${styles.container}`}>
         <h4>{title}</h4>
         {children}
       </div>
