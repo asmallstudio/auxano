2240e0be28fe4ec46cbab37a02d24ac1606d3f7e
Andrew Garcia <runandrew@gmail.com>
2018-06-01 16:16:16 -0700

    update header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,13 @@ import { Router, Link } from "react-static";
 import styles from "./header.scss";
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false
+    };
+  }
+
   // Used to determine active anchor links on the specified path
   _determineHomeHashActive(path, hashLocation) {
     return (match, location) => {
@@ -12,59 +19,98 @@ class Header extends React.Component {
     };
   }
 
+  _handleToggleMenu = () => {
+    this.setState(prevState => ({
+      showMenu: !prevState.showMenu
+    }));
+  };
+
+  _handleMenuClose = () => {
+    console.log("handle close clicked");
+    this.setState({
+      showMenu: false
+    });
+  };
+
   render() {
     return (
       <div className={styles.container}>
         <div className={styles.row}>
-          <div className="col-xs-4">
-            <a href="#">
+          <div
+            className={styles.menuToggleContainer}
+            onClick={this._handleToggleMenu}
+          >
+            <img
+              src="round-close-24-px.svg"
+              width={28}
+              height={28}
+              alt="close-menu-icon"
+            />
+          </div>
+          <div className={styles.logoContainer}>
+            <Link to="#">
               <img
                 src="/uploads/logo.png"
                 alt="a small studio logo"
                 className={styles["logo-image"]}
               />
-            </a>
+            </Link>
           </div>
-          <nav className="right col-xs-8">
-            <ul className={styles.list}>
-              <li>
-                <Link
-                  to="/#our-mission"
-                  activeClassName={styles.activeLink}
-                  isActive={this._determineHomeHashActive("/", "#our-mission")}
-                >
-                  Our Mission
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/#what-we-do"
-                  activeClassName={styles.activeLink}
-                  isActive={this._determineHomeHashActive("/", "#what-we-do")}
-                >
-                  What We Do
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/#our-partners"
-                  activeClassName={styles.activeLink}
-                  isActive={this._determineHomeHashActive("/", "#our-partners")}
-                >
-                  Our Partners
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/#get-involved"
-                  activeClassName={styles.activeLink}
-                  isActive={this._determineHomeHashActive("/", "#get-involved")}
-                >
-                  Get Involved
-                </Link>
-              </li>
-            </ul>
-          </nav>
+          {this.state.showMenu && (
+            <nav>
+              <ul className={styles.list}>
+                <li>
+                  <Link
+                    to="/#our-mission"
+                    activeClassName={styles.activeLink}
+                    isActive={this._determineHomeHashActive(
+                      "/",
+                      "#our-mission"
+                    )}
+                    onClick={this._handleMenuClose}
+                  >
+                    Our Mission
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/#what-we-do"
+                    activeClassName={styles.activeLink}
+                    isActive={this._determineHomeHashActive("/", "#what-we-do")}
+                    onClick={this._handleMenuClose}
+                  >
+                    What We Do
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/#our-partners"
+                    activeClassName={styles.activeLink}
+                    isActive={this._determineHomeHashActive(
+                      "/",
+                      "#our-partners"
+                    )}
+                    onClick={this._handleMenuClose}
+                  >
+                    Our Partners
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/#get-involved"
+                    activeClassName={styles.activeLink}
+                    isActive={this._determineHomeHashActive(
+                      "/",
+                      "#get-involved"
+                    )}
+                    onClick={this._handleMenuClose}
+                  >
+                    Get Involved
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          )}
         </div>
       </div>
     );
