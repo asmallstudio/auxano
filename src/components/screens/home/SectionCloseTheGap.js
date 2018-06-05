7a1e9bdf5cbaae7865e37111b7e8084fe1916886
Andrew Garcia <runandrew@gmail.com>
2018-06-04 17:28:17 -0700

    add anchor padding and fix padding

--- src/components/screens/home/SectionCloseTheGap.js
+++ src/components/screens/home/SectionCloseTheGap.js
@@ -6,7 +6,8 @@ class SectionCloseTheGap extends React.Component {
   render() {
     return (
       <div>
-        <div className={styles.container} id="lets-close-the-gap">
+        <div className={styles.container}>
+          <a class="anchor" id="lets-close-the-gap" />
           <div className={styles.row}>
             <div className="col-xs-12">
               <h1>Let’s close the gap!</h1>
--- src/components/screens/home/SectionOurMission.js
+++ src/components/screens/home/SectionOurMission.js
@@ -6,7 +6,8 @@ class SectionOurMission extends React.Component {
   render() {
     return (
       <div>
-        <div className={styles.container} id="our-mission">
+        <div className={styles.container}>
+          <a class="anchor" id="our-mission" />
           <div className={styles.row}>
             <div className="col-xs-12">
               <h4>Our Mission</h4>
