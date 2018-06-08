eff098fcb9e4b8e44d05943298c94839b349fcdc
Andrew Garcia <runandrew@gmail.com>
2018-06-08 16:24:55 -0700

    clean up

--- src/components/screens/home/Home.js
+++ src/components/screens/home/Home.js
@@ -10,6 +10,15 @@ class Home extends React.Component {
       <div className="container">
         <div className="row">
           <h1>Welcome to your Boilerplate</h1>
+          <hr />
+          <h2>Style Guide</h2>
+          <h1>h1</h1>
+          <h2>h2</h2>
+          <h3>h3</h3>
+          <h4>h4</h4>
+          <h5>h5</h5>
+          <h6>h6</h6>
+          <p>p</p>
         </div>
       </div>
     );
