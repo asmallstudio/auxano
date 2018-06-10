e15373fa07d8c9a5372cfb6a88bf3c32866576cc
Andrew Garcia <runandrew@gmail.com>
2018-06-10 10:05:43 -0700

    update scss

--- src/components/ui/LayoutGuide.js
+++ src/components/ui/LayoutGuide.js
@@ -5,7 +5,7 @@ import styles from "./layoutGuide.scss";
 export default () => (
   <div className={styles.container}>
     <div className={styles.row}>
-      <div className="col-md-4 col-lg-6" style={{ backgroundColor: "orange " }}>
+      <div className="col-xs-4 col-lg-6 col-xs-offset-1 col-lg-offset-0">
         <h2>Layout Guide</h2>
       </div>
     </div>
