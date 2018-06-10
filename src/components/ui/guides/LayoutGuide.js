0b7bdabe5880a5e7dada77ec5508206a2f9f878a
Andrew Garcia <runandrew@gmail.com>
2018-06-10 13:21:11 -0700

    add test and update scss variables

--- src/components/ui/guides/LayoutGuide.js
+++ src/components/ui/guides/LayoutGuide.js
@@ -4,16 +4,28 @@ import styles from "./layoutGuide.scss";
 
 export default () => (
   <div className={styles.container}>
-    <div className={styles.row}>
+    <div className={styles.row} id="layout-row">
       <div className="col-xs-20">
         <h1>Layout Guide</h1>
       </div>
-      <div className="col-xs-20 col-sm-15 col-lg-10">
-        <h3>xs 20; sm 15; lg 10;</h3>
+      <div
+        className="col-xs-20 col-sm-15 col-md-12 col-lg-10 col-xl-5"
+        id="layout-test-a"
+      >
+        <h3>xs 20; sm 15; md 12; lg 10; xl 5</h3>
       </div>
-
-      <div className="col-xs-15 col-sm-offset-4">
-        <h3>xs 15; sm offset 4</h3>
+    </div>
+    <div className={styles.row} id="layout-row-b">
+      <div
+        className="col-xs-1 col-xs-offset-1 col-sm-offset-2 col-md-offset-3 col-lg-offset-4 col-xl-offset-5"
+        id="layout-test-b"
+      >
+        <h3>hi</h3>
+      </div>
+    </div>
+    <div className={styles.row} id="layout-row-c">
+      <div className="col-xs-20 hidden-md-down hidden-lg-up" id="layout-test-c">
+        <h3>hi</h3>
       </div>
     </div>
   </div>
---
+++ src/components/ui/guides/tests/layout.test.js
@@ -0,0 +1,46 @@
+var devices = {
+  smallMobile: {
+    deviceName: "smallMobile",
+    tags: ["smallMobile", "all"],
+    size: "300x1000"
+  },
+  mobile: {
+    deviceName: "mobile",
+    tags: ["mobile", "all"],
+    size: "576x1000"
+  },
+  smallTablet: {
+    deviceName: "smallTablet",
+    tags: ["smallTablet", "all"],
+    size: "768x1000"
+  },
+  tablet: {
+    deviceName: "tablet",
+    tags: ["tablet", "all"],
+    size: "992x1000"
+  },
+  desktop: {
+    deviceName: "desktop",
+    tags: ["desktop", "all"],
+    size: "1440x1000"
+  }
+};
+
+beforeTest(function() {
+  const driver = createDriver("http://localhost:3000", "1024x768");
+  session.put("driver", driver);
+});
+
+forAll(devices, function() {
+  test("Home page on ${deviceName}", function(device) {
+    const driver = session.get("driver");
+    resize(driver, device.size);
+    checkLayout(driver, "layout.gspec", device.tags);
+  });
+});
+
+afterTest(function() {
+  const driver = session.get("driver");
+
+  driver.quit();
+});
