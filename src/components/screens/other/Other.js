fbfec70d816cf5a5d408213cf0128df3a37fddb5
Andrew Garcia <runandrew@gmail.com>
2018-07-09 07:14:53 -0700

    add form

--- src/components/screens/other/Other.js
+++ src/components/screens/other/Other.js
@@ -2,7 +2,34 @@ import React from "react";
 import { withRouteData } from "react-static";
 
 class Other extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  _encode = data => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+  };
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: this._encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
+    const { name, email, message } = this.state;
+
     return (
       <div>
         <div className="container">
@@ -11,6 +38,43 @@ class Other extends React.Component {
               <h1>{this.props.other.other.title}</h1>
               <h3>{this.props.other.other.exampleText}</h3>
               <hr />
+              <form onSubmit={this.handleSubmit}>
+                <p>
+                  <label>
+                    Your Name:{" "}
+                    <input
+                      type="text"
+                      name="name"
+                      value={name}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    Your Email:{" "}
+                    <input
+                      type="email"
+                      name="email"
+                      value={email}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    Message:{" "}
+                    <textarea
+                      name="message"
+                      value={message}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </p>
+                <p>
+                  <button type="submit">Send</button>
+                </p>
+              </form>
             </div>
           </div>
         </div>
