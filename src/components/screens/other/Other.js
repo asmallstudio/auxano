1e9cebc3e466b91d7592eccf60bf5e22215a1590
Andrew Garcia <runandrew@gmail.com>
2018-07-16 15:53:26 -0700

    remove boilerplate code

--- src/components/screens/other/Other.js
+++ src/components/screens/other/Other.js
@@ -4,30 +4,9 @@ import { withRouteData } from "react-static";
 class Other extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: "", email: "", message: "" };
   }
 
-  _encode = data => {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-  };
-
-  handleSubmit = e => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: this._encode({ "form-name": "contact2", ...this.state })
-    });
-
-    e.preventDefault();
-  };
-
-  handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
   render() {
-    const { name, email, message } = this.state;
-
     return (
       <div>
         <div className="container">
@@ -35,57 +14,6 @@ class Other extends React.Component {
             <div className="col-xs-12">
               <h1>{this.props.other.other.title}</h1>
               <h3>{this.props.other.other.exampleText}</h3>
-              <hr />
-              <form
-                onSubmit={this.handleSubmit}
-                name="contact2"
-                netlify="true"
-                action="/other"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-              >
-                <p>
-                  <label>
-                    Don’t fill this out:{" "}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Your Name:{" "}
-                    <input
-                      type="text"
-                      name="name"
-                      value={name}
-                      onChange={this.handleChange}
-                    />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Your Email:{" "}
-                    <input
-                      type="email"
-                      name="email"
-                      value={email}
-                      onChange={this.handleChange}
-                    />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Message:{" "}
-                    <textarea
-                      name="message"
-                      value={message}
-                      onChange={this.handleChange}
-                    />
-                  </label>
-                </p>
-                <p>
-                  <button type="submit">Send</button>
-                </p>
-              </form>
             </div>
           </div>
         </div>
