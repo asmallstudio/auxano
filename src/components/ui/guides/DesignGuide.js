86942c80dd5070cebe9a5d2e1cd57a5decd3027a
Andrew Garcia <runandrew@gmail.com>
2018-07-16 21:03:19 -0700

    fix max column number

--- src/components/ui/guides/DesignGuide.js
+++ src/components/ui/guides/DesignGuide.js
@@ -3,7 +3,7 @@ import React from "react";
 export default () => (
   <div className="container">
     <div className="row">
-      <div className="col-xs-20">
+      <div className="col-xs-12">
         <h2>Style Guide</h2>
         <h1>h1</h1>
         <h2>h2</h2>
