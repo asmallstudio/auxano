1d30917e41a62699de92782c20079c1d7d62d1e1
Scott Smith <mail@ScottHSmith.com>
2018-07-18 16:28:30 -0700

    switch to css vars for theming

--- src/lib/theme.js
+++
@@ -1,9 +0,0 @@
-// NOTE: need to match the variables in the variables.scss
-const theme = {
-  grey: "#808080",
-  gigas: "#483898",
-  fountainBlue: "#50b8b8",
-  white: "#ffffff"
-};
-
-export default theme;
