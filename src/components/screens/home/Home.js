5ea47aa209d32d4cb50f1e5e3d19aa6364078ebd
Andrew Garcia <runandrew@gmail.com>
2018-07-17 19:41:27 -0700

    add vertical spacing

--- src/components/screens/home/Home.js
+++ src/components/screens/home/Home.js
@@ -8,36 +8,34 @@ import styles from "./home.scss";
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <section className="container">
-          <div className="row">
-            <div className={`col-xs-12 col-lg-6 ${styles.titleTextContainer}`}>
-              <div>
-                <h1 className={styles.heroTitle}>
-                  Transcending wealth. Transforming lives.
-                </h1>
-                <p className={styles.heroSubTitle}>
-                  {`We help clients transcend the traditional definition of wealth
+      <section className="container">
+        <div className="row">
+          <div className={`col-xs-12 col-lg-6 ${styles.titleTextContainer}`}>
+            <div>
+              <h1 className={`${styles.heroTitle} vertSpacerSm`}>
+                Transcending wealth. Transforming lives.
+              </h1>
+              <p className={`${styles.heroSubTitle} vertSpacerSm`}>
+                {`We help clients transcend the traditional definition of wealth
                 so it's used to build a legacy that transforms families,
                 finances, and their future.`}
-                  <PrimaryButton className={styles.button}>
-                    CTA Button
-                  </PrimaryButton>
-                </p>
-              </div>
-            </div>
-            <div className="col-xs-12 col-lg-6">
-              <div className={styles.filler} />
-            </div>
-            <div className="col-xs-12">
-              <h1>{this.props.home.hero.title}</h1>
-              <h3>{this.props.home.hero.exampleText}</h3>
-              <hr />
+              </p>
+              <PrimaryButton className={styles.button}>
+                CTA Button
+              </PrimaryButton>
             </div>
           </div>
-        </section>
+          <div className="col-xs-12 col-lg-6">
+            <div className={styles.filler} />
+          </div>
+          <div className="col-xs-12">
+            <h1>{this.props.home.hero.title}</h1>
+            <h3>{this.props.home.hero.exampleText}</h3>
+            <hr />
+          </div>
+        </div>
         <DesignGuide />
-      </div>
+      </section>
     );
   }
 }
