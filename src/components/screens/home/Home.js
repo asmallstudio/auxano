8b3f006d3d24472c6769c4b24f9dbbe731c2339e
Scott Smith <mail@ScottHSmith.com>
2018-07-20 17:55:51 -0700

    added content fields for three column layout

--- src/components/screens/home/Home.js
+++ src/components/screens/home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { withRouteData } from "react-static";
+import { Link } from "react-static";
 
 import DesignGuide from "../../ui/guides/DesignGuide";
 import PrimaryButton from "../../ui/primaryButton/PrimaryButton";
@@ -28,14 +29,38 @@ class Home extends React.Component {
           <div className="col-xs-12 col-lg-6">
             <div className={styles.filler} />
           </div>
+        </div>
+        <div className="row">
           <p className={`${styles.pullquote} col-xs-12 col-md-8`}>
             {this.props.home.pullquote.text}
           </p>
-          <div className="col-xs-12">
-            <h1>{this.props.home.hero.title}</h1>
-            <h3>{this.props.home.hero.exampleText}</h3>
-            <hr />
+        </div>
+        <div className="row">
+          <div className={`${styles.threecols}`}>
+            <div className={`${styles.col} col-xs-12 col-md-4`}>
+              {this.props.home.threecol.col1.heading}
+              {this.props.home.threecol.col1.text}
+              <Link
+                to={this.props.home.threecol.col1.link}
+                className={styles.colLearnMore}
+              >
+                Learn More
+              </Link>
+            </div>
+            <div className={`${styles.col} col-xs-12 col-md-4`}>
+              {this.props.home.threecol.col2.heading}
+              {this.props.home.threecol.col2.text}
+            </div>
+            <div className={`${styles.col} col-xs-12 col-md-4`}>
+              {this.props.home.threecol.col3.heading}
+              {this.props.home.threecol.col3.text}
+            </div>
           </div>
+        </div>
+        <div className="row col-xs-12">
+          <h1>{this.props.home.hero.title}</h1>
+          <h3>{this.props.home.hero.exampleText}</h3>
+          <hr />
         </div>
         <DesignGuide />
       </section>
