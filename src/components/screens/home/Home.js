659ade2b6562a87b2e73ea05118bd936474a6877
Andrew Garcia <runandrew@gmail.com>
2018-07-23 07:49:06 -0700

    add learn more about us

--- src/components/screens/home/Home.js
+++ src/components/screens/home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { withRouteData } from "react-static";
+import { withRouteData, Link } from "react-static";
 
 import PrimaryButton from "../../ui/primaryButton/PrimaryButton";
 import OneThirdColumn from "./OneThirdColumn";
@@ -8,34 +8,45 @@ import styles from "./home.scss";
 class Home extends React.Component {
   render() {
     return (
-      <section className="container">
-        <div className="row">
-          <div className={`col-xs-12 col-lg-6 ${styles.titleTextContainer}`}>
-            <div>
-              <h1 className={`${styles.heroTitle} vertSpacerSm`}>
-                {this.props.home.hero.title}
-              </h1>
-              <p className={`${styles.heroSubTitle} vertSpacerSm`}>
-                {this.props.home.hero.subtitle}
-              </p>
-              <PrimaryButton className={styles.button}>
-                CTA Button
-              </PrimaryButton>
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className={`col-xs-12 col-lg-6 ${styles.titleTextContainer}`}>
+              <div>
+                <h1 className={`${styles.heroTitle} vertSpacerSm`}>
+                  {this.props.home.hero.title}
+                </h1>
+                <p className={`${styles.heroSubTitle} vertSpacerSm`}>
+                  {this.props.home.hero.subtitle}
+                </p>
+                <PrimaryButton className={styles.button}>
+                  CTA Button
+                </PrimaryButton>
+              </div>
+            </div>
+            <div className="col-xs-12 col-lg-6">
+              <div className={styles.filler} />
             </div>
           </div>
-          <div className="col-xs-12 col-lg-6">
-            <div className={styles.filler} />
+          <div className={`${styles.pullquote} row col-xs-12 col-md-8`}>
+            <p>{this.props.home.pullquote.text}</p>
+          </div>
+          <div className={`${styles.threecols} row`}>
+            <OneThirdColumn data={this.props.home.threecol.col1} />
+            <OneThirdColumn data={this.props.home.threecol.col2} />
+            <OneThirdColumn data={this.props.home.threecol.col3} />
           </div>
         </div>
-        <div className={`${styles.pullquote} row col-xs-12 col-md-8`}>
-          <p>{this.props.home.pullquote.text}</p>
+        <div className={`container--fluid ${styles.learnMoreContainer}`}>
+          <div className={`row ${styles.learnMoreRow}`}>
+            <div className="col-xs-12">
+              <Link to="#" className={styles.learnMoreLink}>
+                Learn more about us
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className={`${styles.threecols} row`}>
-          <OneThirdColumn data={this.props.home.threecol.col1} />
-          <OneThirdColumn data={this.props.home.threecol.col2} />
-          <OneThirdColumn data={this.props.home.threecol.col3} />
-        </div>
-      </section>
+      </div>
     );
   }
 }
