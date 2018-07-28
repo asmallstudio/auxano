0ee0270ba3f7efc3c1879debb37f4cdc9aa05389
timbresson <timbresson@gmail.com>
2018-07-28 10:43:39 -0400

    Initial variable set-up

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,21 @@
 import React from "react";
+//import { Link } from "react-static";
+
+import styles from "./footer.scss";
 
 class Footer extends React.Component {
   render() {
     return (
       <div>
-        <div className="container">
-          <div className="row">
+        <div className={`container ${styles.footerContainer}`}>
+          <div className="rowLinks">
             <div className="col-xs-12">
               <h4>[Footer]</h4>
+            </div>
+          </div>
+          <div className="rowInfo">
+            <div className="col-xs-12">
+              <n2>Hello This is Info with stuff</n2>
             </div>
           </div>
         </div>
