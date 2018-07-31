607284a9c1ae72111a3fb1f90f61e3ce098544ac
Scott Smith <mail@ScottHSmith.com>
2018-07-31 14:54:31 -0700

    improved header's ability to contain logo

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import { Router } from "react-static";
 import { hot } from "react-hot-loader";
 
-//
 import Routes from "react-static-routes";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
