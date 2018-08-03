1f5d89cdccd47c615a6c243755082748ae5494d4
Scott Smith <mail@ScottHSmith.com>
2018-08-03 13:12:26 -0700

    add contact form

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -3,6 +3,8 @@ import { withRouteData, Head } from "react-static";
 import { getFullPageTitle } from "../../../lib/utils/copy";
 
 import styles from "./contact.scss";
+import DefaultInput from "../../ui/defaultInput/DefaultInput";
+import DefaultTextArea from "../../ui/defaultTextArea/DefaultTextArea";
 import SubscribeSection from "../../ui/subscribeSection/SubscribeSection";
 
 class Contact extends React.Component {
@@ -21,12 +23,46 @@ class Contact extends React.Component {
         <div className="container">
           <h1 className="sr-text">{contact.pageTitle}</h1>
           <div className="row">
-            <div className="col-xs-12 col-md-6">
+            <section
+              className={`col-xs-12 col-md-6 ${styles.contactSection} ${
+                styles.contactForm
+              }`}
+            >
               <h2>{contact.form.title}</h2>
-            </div>
-            <div className="col-xs-12 col-md-6">
+              <form>
+                <label htmlFor="name-input">Name</label>
+                <DefaultInput
+                  id="name-input"
+                  placeholder="Alex Garcia"
+                  type="text"
+                  autoComplete="name"
+                  className={styles.input}
+                />
+                <label htmlFor="email-input">Email</label>
+                <DefaultInput
+                  id="email-input"
+                  placeholder="alex@example.com"
+                  type="email"
+                  autoComplete="email"
+                  className={styles.input}
+                />
+                <label htmlFor="subject-input">Subject</label>
+                <DefaultInput
+                  id="subject-input"
+                  type="text"
+                  className={styles.input}
+                />
+                <label htmlFor="body-input">Your Message</label>
+                <DefaultTextArea id="body-input" className={styles.input} />
+              </form>
+            </section>
+            <section
+              className={`col-xs-12 col-md-5 col-md-offset-1 ${
+                styles.contactSection
+              } ${styles.contactInfo}`}
+            >
               <h2>{contact.info.title}</h2>
-            </div>
+            </section>
           </div>
         </div>
         <SubscribeSection
