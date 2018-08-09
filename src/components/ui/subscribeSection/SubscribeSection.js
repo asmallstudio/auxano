0814fa6ea35db2cf4831430bf5370751d989391a
Scott Smith <mail@ScottHSmith.com>
2018-08-09 10:02:42 -0700

    start trying to make fetch a thing

--- src/components/ui/subscribeSection/SubscribeSection.js
+++ src/components/ui/subscribeSection/SubscribeSection.js
@@ -4,6 +4,52 @@ import styles from "./subscribeSection.scss";
 import DefaultInput from "../defaultInput/DefaultInput";
 import PrimaryButton from "../primaryButton/PrimaryButton";
 
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
+class SubscribeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: "" };
+  }
+
+  /* Here’s the juicy bit for posting the form submission */
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "subscribe", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { email } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <DefaultInput
+          placeholder="Email"
+          type="email"
+          aria-label="newsletter email"
+          autoComplete="email"
+          className={styles.input}
+          value={email}
+          onChange={this.handleChange}
+        />
+        <PrimaryButton className={`${styles.button}`}>Subscribe</PrimaryButton>
+      </form>
+    );
+  }
+}
+
 class SubscribeSection extends React.Component {
   render() {
     const { heading, text } = this.props;
@@ -14,25 +60,11 @@ class SubscribeSection extends React.Component {
             <h2 className="style-as-h3">{heading}</h2>
             <p>{text}</p>
           </div>
-          <form
-            name="subscribe"
-            method="POST"
-            netlify
+          <SubscribeForm
             className={`col-xs-12 col-md-10 col-md-offset-1 col-lg-6 col-lg-offset-1 ${
               styles.action
             }`}
-          >
-            <DefaultInput
-              placeholder="Email"
-              type="email"
-              aria-label="newsletter email"
-              autoComplete="email"
-              className={styles.input}
-            />
-            <PrimaryButton className={`${styles.button}`}>
-              Subscribe
-            </PrimaryButton>
-          </form>
+          />
         </div>
       </div>
     );
