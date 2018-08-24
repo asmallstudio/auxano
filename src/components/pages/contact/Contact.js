e9f661b90cfbe3629e5b1264f2de0461923138ec
Scott Smith <mail@ScottHSmith.com>
2018-08-24 11:00:15 -0700

    change map size

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -198,87 +198,89 @@ class Contact extends React.Component {
         <Head>
           <title>{getFullPageTitle(contact.pageTitle)}</title>
         </Head>
-        <div className="container">
-          <h1 className="sr-text">{contact.pageTitle}</h1>
-          <div className="row">
-            <section
-              className={`col-xs-12 col-md-6 ${styles.contactSection} ${
-                styles.contactForm
-              }`}
-            >
-              <h2>{contact.form.title}</h2>
-              <ContactForm />
-            </section>
-            <section
-              className={`col-xs-12 col-md-5 col-md-offset-1 ${
-                styles.contactSection
-              } ${styles.contactInfo}`}
-            >
-              <h2>{contact.info.title}</h2>
-              <address itemScope itemType="http://schema.org/Organization">
-                <meta itemProp="name" content="Auxano Advisors" />
-                <meta itemProp="url" content={constants.siteMeta.url} />
-                <p className={`style-as-h3 ${styles.contactInfoHeader}`}>
-                  Email
-                </p>
-                <a href="mailto:info@auxanoadvisors.com" itemProp="email">
-                  info@auxanoadvisors.com
-                </a>
-                <p className={`style-as-h3 ${styles.contactInfoHeader}`}>
-                  Phone
-                </p>
-                <a href="tel:+14258891261" itemProp="telephone">
-                  (425) 889-1261
-                </a>
-                <p className={`style-as-h3 ${styles.contactInfoHeader}`}>
-                  Address
-                </p>
-                <p
-                  itemProp="address"
-                  itemScope
-                  itemType="http://schema.org/PostalAddress"
-                >
-                  <span itemProp="streetAddress">
-                    10900 NE 4th Street
-                    <br />
-                    Suite 1950
-                  </span>
-                  <br />
-                  <span itemProp="addressLocality">Bellevue</span>,{" "}
-                  <span itemProp="addressRegion">WA</span>{" "}
-                  <span itemProp="postalCode">98004</span>
-                  <meta itemProp="addressCountry" content="US" />
-                </p>
-              </address>
-            </section>
-          </div>
-        </div>
         <div className={`container--fluid ${styles.dingusDotContainer}`}>
           <div className="container">
-            <div className={`row ${styles.dingusDotRelative}`}>
-              <section className={`col-xs-12 ${styles.mapSection}`}>
-                <IFrame
-                  url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.6162509396745!2d-122.19616678423552!3d47.61415107918512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906c89d25fcd9b%3A0x2b68c721fc3f0f59!2sAuxano+Advisors%2C+LLC!5e0!3m2!1sen!2sus!4v1533505977008"
-                  width="100%"
-                  position="relative"
-                  frameBorder="0"
-                  className={styles.mapEmbed}
-                  style={{ border: 0 }}
-                />
-                <p className={styles.mapLinks}>
-                  <Link to="https://goo.gl/maps/y3WodKyfL7w">
-                    Open in Google Maps
-                  </Link>
-                  <br />
-                  <Link to="https://maps.apple.com/?address=10900%20NE%204th%20St,%20Unit%201950,%20Bellevue,%20WA%20%2098004,%20United%20States&auid=4419463368018388351&ll=47.614058,-122.194218&lsp=9902&q=Auxano%20Advisors&t=m">
-                    Open in Apple Maps
-                  </Link>
-                </p>
+            <h1 className="sr-text">{contact.pageTitle}</h1>
+            <div className="row">
+              <section
+                className={`col-xs-12 col-md-6 ${styles.contactSection} ${
+                  styles.contactForm
+                }`}
+              >
+                <h2>{contact.form.title}</h2>
+                <ContactForm />
               </section>
-              <div className={styles.dingusDot} />
+              <section
+                className={`col-xs-12 col-md-5 col-md-offset-1 ${
+                  styles.contactSection
+                } ${styles.contactInfo}`}
+              >
+                <h2>{contact.info.title}</h2>
+                <address itemScope itemType="http://schema.org/Organization">
+                  <div className={styles.contactInfoSection}>
+                    <meta itemProp="name" content="Auxano Advisors" />
+                    <meta itemProp="url" content={constants.siteMeta.url} />
+                    <h3 className="sr-text">Email</h3>
+                    <a href="mailto:info@auxanoadvisors.com" itemProp="email">
+                      info@auxanoadvisors.com
+                    </a>
+                  </div>
+                  <div className={styles.contactInfoSection}>
+                    <h3 className="sr-text">Phone</h3>
+                    <a href="tel:+14258891261" itemProp="telephone">
+                      (425) 889-1261
+                    </a>
+                  </div>
+                  <div className={styles.contactInfoSection}>
+                    <h3 className="sr-text">Address</h3>
+                    <p
+                      itemProp="address"
+                      itemScope
+                      itemType="http://schema.org/PostalAddress"
+                    >
+                      <span itemProp="streetAddress">
+                        10900 NE 4th Street
+                        <br />
+                        Suite 1950
+                      </span>
+                      <br />
+                      <span itemProp="addressLocality">Bellevue</span>,{" "}
+                      <span itemProp="addressRegion">WA</span>{" "}
+                      <span itemProp="postalCode">98004</span>
+                      <meta itemProp="addressCountry" content="US" />
+                    </p>
+                  </div>
+                </address>
+                <section className={`col-xs-12 ${styles.mapSection}`} />
+                <div
+                  className={`${styles.contactInfoSection} ${
+                    styles.dingusDotRelative
+                  }`}
+                >
+                  <IFrame
+                    url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.6162509396745!2d-122.19616678423552!3d47.61415107918512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906c89d25fcd9b%3A0x2b68c721fc3f0f59!2sAuxano+Advisors%2C+LLC!5e0!3m2!1sen!2sus!4v1533505977008"
+                    width="100%"
+                    position="relative"
+                    frameBorder="0"
+                    className={styles.mapEmbed}
+                    style={{ border: 0 }}
+                  />
+                  <p className={styles.mapLinks}>
+                    <Link to="https://goo.gl/maps/y3WodKyfL7w">
+                      Open in Google Maps
+                    </Link>
+                    <br />
+                    <Link to="https://maps.apple.com/?address=10900%20NE%204th%20St,%20Unit%201950,%20Bellevue,%20WA%20%2098004,%20United%20States&auid=4419463368018388351&ll=47.614058,-122.194218&lsp=9902&q=Auxano%20Advisors&t=m">
+                      Open in Apple Maps
+                    </Link>
+                  </p>
+                  <div className={styles.dingusDot} />
+                </div>
+              </section>
             </div>
           </div>
         </div>
+
         <div className="container--fluid">
           <img
             className={styles.imageDivider}
