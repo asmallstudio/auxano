90473e647f74dd94a52ba69d46c121b8531adf70
Scott Smith <mail@ScottHSmith.com>
2018-08-28 14:13:42 -0700

    fix wrong typeface

--- src/components/ui/oneThirdColumn/OneThirdColumn.js
+++ src/components/ui/oneThirdColumn/OneThirdColumn.js
@@ -9,7 +9,7 @@ class OneThirdColumn extends React.Component {
     return (
       <div className={`${styles.col} col-xs-12 col-md-4`}>
         <img className={styles.colIcon} src={data.icon} alt="" />
-        <h2 className="style-as-h3">{data.heading}</h2>
+        <h2>{data.heading}</h2>
         <p>{data.text}</p>
         <LinkWithArrow to={data.link} className={styles.colLearnMore}>
           Learn More
