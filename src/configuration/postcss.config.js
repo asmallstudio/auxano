d033103f4630fc4b43820f1caa096c008decd424
Scott Smith <mail@ScottHSmith.com>
2018-09-19 12:29:57 -0700

    fix race condition?

--- src/configuration/postcss.config.js
+++ src/configuration/postcss.config.js
@@ -3,11 +3,16 @@ module.exports = {
   sourceMap: true,
   ident: "postcss",
   plugins: {
-    "postcss-import": {},
-    "postcss-normalize": {},
+    // Exports to file that is then imported.
     "postcss-custom-properties": {
       importFrom: "./src/lib/theme.json",
       exportTo: "./src/components/styles/theme.css"
+    },
+    "postcss-import": {},
+    "postcss-normalize": {},
+    // eslint-disable-next-line no-dupe-keys
+    "postcss-custom-properties": {
+      importFrom: "./src/lib/theme.json"
     },
     "postcss-flexbugs-fixes": {},
     autoprefixer: {
