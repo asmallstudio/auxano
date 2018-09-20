fa64e466be91dd784d64aac0561e45b7171245a4
Scott Smith <mail@ScottHSmith.com>
2018-09-20 14:25:07 -0700

    add three columns to home page

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -5,6 +5,7 @@ import { pageChange } from "../../../lib/utils/pageChange";
 import styles from "./home.scss";
 import CallToActionLink from "../../ui/callToActionLink/CallToActionLink";
 import FullWidthSectionText from "../../ui/fullWidthSectionText/FullWidthSectionText";
+import OneThirdColumn from "../../ui/oneThirdColumn/OneThirdColumn";
 import FullWidthSectionActionLink from "../../ui/fullWidthSectionActionLink/FullWidthSectionActionLink";
 import SubscribeSection from "../../ui/subscribeSection/SubscribeSection";
 
@@ -41,6 +42,13 @@ class Home extends React.Component {
           </div>
         </div>
         <FullWidthSectionText text={home.pullquote} className="dg-pullquote" />
+        <section className="container dg-services">
+          <div className={`${styles.threecols} row`}>
+            <OneThirdColumn data={home.callouts.item1} className="dg-item1" />
+            <OneThirdColumn data={home.callouts.item2} className="dg-item2" />
+            <OneThirdColumn data={home.callouts.item3} className="dg-item3" />
+          </div>
+        </section>
         <FullWidthSectionActionLink
           linkText={home.learnMore.text}
           to={home.learnMore.link}
