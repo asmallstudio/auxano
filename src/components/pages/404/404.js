64c09a26d5f4210c47783f2fead852d4cfc2e42a
Scott Smith <mail@ScottHSmith.com>
2018-09-21 13:12:18 -0700

    improve 404 ux

--- src/components/pages/404/404.js
+++ src/components/pages/404/404.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { Link, Head } from "react-static";
+import { getFullPageTitle } from "../../../lib/utils/copy";
+import { pageChange } from "../../../lib/utils/pageChange";
+
+import styles from "./404.scss";
 
 class PageNotFound extends React.Component {
+  componentDidMount() {
+    pageChange();
+  }
+
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <h2>{"404 - Oh no's! We couldn't find that page :("}</h2>
+      <React.Fragment>
+        <Head>
+          <title>{getFullPageTitle("Page not found")}</title>
+        </Head>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              <h1 className={styles.title}>That page cannot be found.</h1>
+              <p className={styles.explainer}>
+                Try going <Link to="/">home</Link>.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
