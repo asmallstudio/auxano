f49bd4f3ae55f48ac5556bc1580aff38b257b545
Andrew Garcia <runandrew@gmail.com>
2018-09-22 19:53:05 -0700

    add utility function for formatting date strings to "from now"

--- src/lib/utils/copy.js
+++ src/lib/utils/copy.js
@@ -1,5 +1,12 @@
+import moment from "moment";
+
 import constants from "../constants.json";
 
 const getFullPageTitle = title => `${title} | ${constants.siteMeta.title}`;
 
-export { getFullPageTitle };
+const formatDateStringToFromNow = dateString => {
+  const momentDate = moment(dateString);
+  return momentDate.fromNow();
+};
+
+export { getFullPageTitle, formatDateStringToFromNow };
