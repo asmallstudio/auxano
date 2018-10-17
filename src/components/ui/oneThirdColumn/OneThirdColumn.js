db93c0219e4109747c394591cf8f5905007cf4a2
Scott Smith <mail@ScottHSmith.com>
2018-10-16 21:46:43 -0500

    make icons clickable with link

--- src/components/ui/oneThirdColumn/OneThirdColumn.js
+++ src/components/ui/oneThirdColumn/OneThirdColumn.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-static";
 
 import LinkWithArrow from "../linkWithArrow/LinkWithArrow";
 import styles from "./oneThirdColumn.scss";
@@ -8,9 +9,16 @@ class OneThirdColumn extends React.Component {
     const { data, className = "" } = this.props;
     return (
       <div className={`${styles.col} col-xs-12 col-md-4 ${className}`}>
-        {typeof data.icon !== "undefined" && (
-          <img className={styles.colIcon} src={data.icon} alt="" />
-        )}
+        {typeof data.icon !== "undefined" &&
+          typeof data.link === "undefined" && (
+            <img className={styles.colIcon} src={data.icon} alt="" />
+          )}
+        {typeof data.icon !== "undefined" &&
+          typeof data.link !== "undefined" && (
+            <Link to={data.link}>
+              <img className={styles.colIcon} src={data.icon} alt="" />
+            </Link>
+          )}
         <h2>{data.heading}</h2>
         <p>{data.text}</p>
         {typeof data.link !== "undefined" && (
