f23b3d5fe8d6b7c4b673c5c6a288eb0904f34b5a
Scott Smith <mail@ScottHSmith.com>
2019-01-17 11:56:55 -0800

    Update Generations page layout

--- src/components/pages/generations/Generations.js
+++ src/components/pages/generations/Generations.js
@@ -9,6 +9,7 @@ import {
 
 import styles from "./generations.scss";
 import Markdown from "react-markdown";
+import FullWidthSectionText from "../../ui/fullWidthSectionText/FullWidthSectionText";
 import FullWidthSectionActionLink from "../../ui/fullWidthSectionActionLink/FullWidthSectionActionLink";
 import SubscribeSection from "../../ui/subscribeSection/SubscribeSection";
 
@@ -39,30 +40,52 @@ class Generations extends React.Component {
         <section className={`container--fluid ${styles.heroContainer} dg-hero`}>
           <div className="container">
             <div className="row">
-              <div className={`col-xs-12 col-md-6 ${styles.heroTextContainer}`}>
+              <div className={`col-xs-12 col-md-9 ${styles.heroTextContainer}`}>
                 <h1>{generations.hero.title}</h1>
-                <Markdown source={generations.hero.subtitle} />
+                <p className="style-as-h2">{generations.hero.subtitle}</p>
               </div>
+            </div>
+          </div>
+        </section>
+        <FullWidthSectionText
+          text={generations.body.description1}
+          className={`dg-body ${styles.pageBody}`}
+        />
+        <section className="container">
+          <div className="row">
+            <div className={`col-xs-12 ${styles.textPlain}`}>
+              <Markdown
+                allowedTypes={["text", "paragraph", "emphasis", "strong"]}
+                className={styles.pageBody}
+              >
+                {generations.body.description2}
+              </Markdown>
             </div>
           </div>
         </section>
         <section className="container">
           <div className="row">
-            <div className={`col-xs-12 col-md-8 ${styles.pageBody}`}>
-              <Markdown source={generations.body.description} />
+            <div className="col-xs-12">
+              <h2 className={styles.optionsHeader}>
+                {generations.body.options.heading}
+              </h2>
             </div>
-            <h2>{generations.body.options.heading}</h2>
-            <ul>
-              {generations.body.options.list.map((item, i) => (
-                <li key={i}>
-                  <Markdown allowedTypes={["text", "emphasis", "strong"]} />
-                  {item}
-                </li>
-              ))}
-            </ul>
-            <p>{generations.body.closing}</p>
+            <div className="row">
+              <ul className={`col-xs-12 ${styles.textList}`}>
+                {generations.body.options.list.map((item, i) => (
+                  <li key={i} className="style-as-p">
+                    <Markdown allowedTypes={["text", "emphasis", "strong"]} />
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </section>
+        <FullWidthSectionText
+          text={generations.body.closing}
+          className={styles.pageBodyClosing}
+        />
         <FullWidthSectionActionLink
           linkText={pickFirstAvailableString(
             generations.actionBanner.text,
