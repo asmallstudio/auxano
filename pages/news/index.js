45912423a5e2f1f2a19067ad78dcb42b4e7ed87e
Scott Smith <mail@ScottHSmith.com>
2020-11-05 22:11:36 +0000

    Switch to new articles design.

--- pages/news/index.js
+++ pages/news/index.js
@@ -6,76 +6,18 @@ import {
   createSlugFromTitleAndDate,
 } from "@asmallstudio/utilities";
 import { getDirectory, getSingleFileJson } from "@asmallstudio/utilities/node";
-import { NextAmbiLink } from "@asmallstudio/components";
 import constants from "lib/constants.json";
 
-// import JournalEntryItem from "components/ui/journalEntryItem/JournalEntryItem";
-import LinkWithArrow from "components/ui/linkWithArrow/LinkWithArrow";
+import JournalEntryItem from "components/ui/journalEntryItem/JournalEntryItem";
 import styles from "./index.module.scss";
 
 export default function Index({ siteData, posts }) {
   return (
     <>
       <Head>
-        <title>{getFullPageTitle("News", constants.siteMeta.title)}</title>
+        <title>{getFullPageTitle("Articles", constants.siteMeta.title)}</title>
         <meta name="description" content={siteData.siteDescription} />
       </Head>
-      <div className={`container ${styles.articlePage}`}>
-        <div className="row">
-          <div className={`col-xs-12 ${styles.articleList}`}>
-            <h1 className="style-as-h3">News & Resources</h1>
-            <ul>
-              {posts.map((post) => (
-                <li key={post.slug} className={styles.indexArticle}>
-                  <div className={styles.titleAndLinkContainer}>
-                    <NextAmbiLink
-                      href="/news/[post]"
-                      as={`/news/${post.slug}`}
-                      className={styles.postTitle}
-                    >
-                      {post.title}
-                    </NextAmbiLink>
-                    <LinkWithArrow
-                      to="/news/[post]"
-                      as={`/news/${post.slug}`}
-                      className={styles.arrowLink}
-                    >
-                      Read more
-                    </LinkWithArrow>
-                  </div>
-                  <div className={styles.metaInfoContainer}>
-                    <p>{formatRelativeDateString(post.date)}</p>
-                  </div>
-                </li>
-              ))}
-            </ul>
-
-            {/* totalPages > 1 && (
-            <nav className={styles.pagination}>
-              <h3>Pages</h3>
-              <ul className={styles.pageList}>
-                {Array.from(new Array(totalPages), (d, i) => i).map(
-                  page => {
-                    const nextPage = page + 1;
-                    return (
-                      <li key={nextPage}>
-                        <PaginationLink
-                          to={`/news/page/${nextPage}`}
-                          isCurrent={nextPage === currentPage}
-                        >
-                          {nextPage}
-                        </PaginationLink>
-                      </li>
-                    );
-                  }
-                )}
-              </ul>
-            </nav>
-          ) */}
-          </div>
-        </div>
-      </div>
-      {/* 
       <div className={`container ${styles.articlePage}`}>
         <div className="row">
           <div className="col-xs-12">
@@ -94,7 +36,7 @@ export default function Index({ siteData, posts }) {
             />
           ))}
         </ul>
-
+        {/* 
         totalPages > 1 && (
               <nav className={styles.pagination}>
                 <h3>Pages</h3>
@@ -119,6 +61,7 @@ export default function Index({ siteData, posts }) {
             )
       </div>
       */}
+      </div>
     </>
   );
 }
